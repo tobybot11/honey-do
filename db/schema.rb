7005f4ba6a0fd4d4593eb58c4029de25443fb670
Tobias Ford <tford@Macintosh-7.local>
2009-03-18 17:14:34 -0400

    db:migrate after task created

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20090318211414) do
+
+  create_table "tasks", :force => true do |t|
+    t.string   "name"
+    t.integer  "priority"
+    t.boolean  "completed"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
